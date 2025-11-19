99bf050b2e237b76b1babe4a5e2a1852494e66a9
BenjaminAUM <bpowel11@aum.edu>
2025-11-19 10:08:42 -0600

    Add files via upload

---
+++ math/math/math.cpp
@@ -0,0 +1,17 @@
+// math
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int First_number;  //First Variable
+	First_number = 23; 
+	int Second_number; //Second Variable
+	Second_number = 15;
+	int Sum; //Result of both Variable's added together
+
+	Sum = First_number + Second_number;
+	
+	cout << "The sum of " << First_number << " + " << Second_number << " is " << Sum << endl;
+	return 0;
+}
