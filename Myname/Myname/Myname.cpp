82ec12394f14b010cd77afe3cbbcfafc04ed68ea
BenjaminAUM <bpowel11@aum.edu>
2025-11-19 10:07:40 -0600

    Add files via upload

---
+++ Myname/Myname/Myname.cpp
@@ -0,0 +1,11 @@
+//My name
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Benjamin Powell" << endl;
+	cout << endl;
+	cout << "He has multiply sibblings, being the youngest of 5. He loves computers, spends a majority of his time when he isn't at the gym. On his computer, either playing video games or watching youtubbe/movies. He built the computer that he uses right now to play games on, with a little help from his father." << endl;
+	return 0;
+}
